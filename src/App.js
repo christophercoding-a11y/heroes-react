b43ed01032ad4f6f758143c145a6ea94627357ec
christophercodinga11y <christopherjack14@gmail.com>
2025-05-21 10:40:37 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router"
+
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+import Error from "./components/Error"
+
+
+
+const App =()=> {
+
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={ <Main />} />
+                <Route path="*" element={< Error />} />
+            </Routes>
+            <Footer />
+        </>
+    )
+}
+
+export default App
